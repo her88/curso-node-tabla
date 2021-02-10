d237d1b01f88664592baa167a27418847cc894d8
hperez <hernan.perez@gire.com>
2021-02-10 16:31:11 -0300

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const crearArchivo = async (base = 5, listar = false, hasta = 10) => {  
+    try {    
+        let salida = '';
+
+        for (let i = 1; i <= hasta; i++) {
+            salida += `${base} X ${i} = ${base * i}\n`;
+        }
+
+        if (listar) {
+            console.log('==============');
+            console.log(`   Tabla del ${base}     `);
+            console.log('==============');
+
+            console.log(salida);
+        }
+
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+        return `tabla-${base}.txt`;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    crearArchivo
+}
